974fe25b9cd50f4b69827f015cb8080c4f64890c
kmtvar <keisuke4270131@icloud.com>
2019-11-27 20:18:54 +0900

    [add]
    Added user left announcement

--- Server_Programs/server_funs.py
+++ Server_Programs/server_funs.py
@@ -3,10 +3,15 @@ def new_client_welcome(client, server) :
         server.send_message(client,f.read())
 
 def send_messages(client, server, message) :
-    if message == "--help" :
+    if message == "" :
+        pass
+    elif message == "--help" :
         with open("options.txt", "rb") as f :
             server.send_message(client,f.read())
     else :
-        user_id = f"User{client['id']}>>"
-        server.send_message_to_all_except(user_id + message, client)
+        user_id = f"  <User{client['id']}>"
+        server.send_message_to_all_except(message + user_id, client)
 
+def bye_client(client, server) :
+    user_left_announcement = f"### User{client['id']} has left ###"
+    server.send_message_to_all_except(user_left_announcement, client)
