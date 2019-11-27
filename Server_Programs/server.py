0b36135785554a2eb5dd1702350061f5cf456f26
kmtvar <keisuke4270131@icloud.com>
2019-11-27 20:18:09 +0900

    [add]
    Added user left announcement

--- Server_Programs/server.py
+++ Server_Programs/server.py
@@ -2,9 +2,10 @@ from websocket_server import WebsocketServer
 import server_funs
 
 port = 12345
-ip_addr = '192.168.0.12'
+ip_addr = '127.0.0.1'
 
 server = WebsocketServer(port,host=ip_addr)
 server.set_fn_new_client(server_funs.new_client_welcome)
 server.set_fn_message_received(server_funs.send_messages)
+server.set_fn_client_left(server_funs.bye_client)
 server.run_forever()
