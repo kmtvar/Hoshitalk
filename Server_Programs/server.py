150e710d6b8e305ec258b866dcc80b6ab4208824
kmtvar <keisuke4270131@icloud.com>
2019-11-29 01:02:09 +0900

    [add]
    ssl

--- Server_Programs/server.py
+++ Server_Programs/server.py
@@ -4,7 +4,7 @@ import server_funs
 port = 12345
 ip_addr = '127.0.0.1'
 
-server = WebsocketServer(port,host=ip_addr)
+server = WebsocketServer(port,host=ip_addr,key="key.pem",cert="cert.pem")
 server.set_fn_new_client(server_funs.new_client_welcome)
 server.set_fn_message_received(server_funs.send_messages)
 server.set_fn_client_left(server_funs.bye_client)
